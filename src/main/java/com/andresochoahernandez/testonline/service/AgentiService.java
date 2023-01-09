9ccb123018c8775b9f9038cf5b1345b4fa458301
andres <ada.wong.gv@gmail.com>
2023-01-09 16:24:23 +0100

    code cleanup

--- src/main/java/com/andresochoahernandez/testonline/service/AgentiService.java
+++ src/main/java/com/andresochoahernandez/testonline/service/AgentiService.java
@@ -15,42 +15,53 @@ import java.util.Collection;
 @Service
 public class AgentiService implements UserDetailsService {
     private final UserRepository users;
-    public AgentiService(UserRepository users) {this.users = users;}
+
+    public AgentiService(UserRepository users) {
+        this.users = users;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return users.findByUsername(username).map(SecureUser::new).orElseThrow(() -> new UsernameNotFoundException("L'username inserito non è presente nel sistema " + username));
     }
 
-    private class SecureUser implements UserDetails{
-        private User user;
-        public SecureUser(User user){
+    private class SecureUser implements UserDetails {
+        private final User user;
+
+        public SecureUser(User user) {
             this.user = user;
         }
+
         @Override
         public Collection<? extends GrantedAuthority> getAuthorities() {
             return Arrays.stream(this.user.getRoles().split(",")).map(SimpleGrantedAuthority::new).toList();
         }
+
         @Override
         public String getPassword() {
             return this.user.getPassword();
         }
+
         @Override
         public String getUsername() {
             return this.user.getUsername();
         }
+
         @Override
         public boolean isAccountNonExpired() {
             return true;
         }
+
         @Override
         public boolean isAccountNonLocked() {
             return true;
         }
+
         @Override
         public boolean isCredentialsNonExpired() {
             return true;
         }
+
         @Override
         public boolean isEnabled() {
             return true;
