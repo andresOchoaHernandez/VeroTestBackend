f0fdf51329838b371bcb00903d5eea8c26167313
andres <ada.wong.gv@gmail.com>
2023-01-13 14:37:03 +0100

    commit before attempt a major change in the project. All frontend will be handle by the react frontend, including login page. Up until now it's been handled by springboot

--- src/main/java/com/andresochoahernandez/testonline/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/andresochoahernandez/testonline/configuration/security/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.andresochoahernandez.testonline.configuration.security;
 import com.andresochoahernandez.testonline.service.AgentiService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
